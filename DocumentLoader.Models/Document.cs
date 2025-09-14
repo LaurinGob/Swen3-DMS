23bd863d967a159901eba33b2dd8512dbe4656f5
Sophie Peirl <sophie.peirl@gmail.com>
2025-09-14 14:18:45 +0200

    DocumentLoader.Models ordner struktur fix

---
+++ DocumentLoader.Models/Document.cs
@@ -0,0 +1,15 @@
+﻿namespace DocumentLoader.Models
+{
+    public class Document
+    {
+        //test
+        public int Id { get; set; }
+        public string FileName { get; set; } = string.Empty;
+
+        public string Summary { get; set; } = string.Empty;
+
+        public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+
+        public string FilePath { get; set; } = string.Empty;
+    }
+}
