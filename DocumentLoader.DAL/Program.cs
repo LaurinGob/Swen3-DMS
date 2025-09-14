97578c9eb6d00dfd60d29747e4c97d77b7d2acaf
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2025-09-14 17:04:17 +0200

    cleanup for first sprint

--- DocumentLoader.DAL/Program.cs
+++ DocumentLoader.DAL/Program.cs
@@ -10,7 +10,6 @@ namespace DocumentLoader.DAL
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
