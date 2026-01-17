3ca77edf2cc9022951c2c4344004a091fd875a07
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2026-01-17 16:28:51 +0100

    ElasticSearch

---
+++ DocumentLoader.ElasticSearch/LogstashLogger.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+public static class LogstashLogger
+{
+    private static readonly string host = "logstash"; // service name in docker-compose
+    private static readonly int port = 5044;
+
+    public static void Log(object logEntry)
+    {
+        var json = JsonSerializer.Serialize(logEntry);
+        var data = Encoding.UTF8.GetBytes(json + "\n");
+
+        using var client = new TcpClient(host, port);
+        client.GetStream().Write(data, 0, data.Length);
+    }
+}
+
+/*
+LogstashLogger ist ein Singleton, überall im code verwendbar
+
+Beispielcode:
+
+LogstashLogger.Log(new
+{
+    timestamp = DateTime.UtcNow,
+    service = "api",
+    level = "info",
+    message = "Document processed",
+    documentId = Guid.NewGuid()
+});
+
+*/
